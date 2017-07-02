8ca2d85205de0372f545fed7425130bdcacd0b88
siddhant kumar singh <sid10on10@gmail.com>
2017-07-03 00:54:45 +0530

    commit message

---
+++ student-parliament/committees.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>Students' Parliament Portal | IIT (BHU) Varanasi</title>
+
+        <!-- Bootstrap minified CSS -->
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>        
+        <!-- News Ticker CSS -->
+        <link href="assets/css/news-ticker.css" rel="stylesheet" type="text/css"/>
+        <!--Custom stylesheet-->
+        <link href="assets/css/custom.css" rel="stylesheet" type="text/css"/>  
+
+        <style type="text/css">
+
+
+        </style>
+
+    </head>
+
+    <body>
+        <div class="top-line"></div>
+
+        <?php include_once("header.php"); ?>
+        <?php include_once("navigation.php"); ?>
+
+        <div class="wrapper bg2">
+            <div class="container bg padding-bottom-20">
+
+                <div class="col-md-12 col-lg-12 margin-bottom-20 text-justify font-arya font-size-1point2em"><!--START Left hand side area-->
+
+<div class="committee">				
+<h4>Parliamentary Affairs Committee</h4>
+
+The Parliamentary Affairs Committee is responsible for deciding the
+
+agenda of the Parliament meetings. It also server the purpose of  amending and bringing 
+
+up-to-date all rules, procedures, structures, norms, convention, codes-of-conduct, etc. as 
+
+well as study all proposed changes in these rules and procedures and give its opinion on
+
+the same.
+
+<h4>Finance Committee</h4>
+
+The Principal aim of the Finance Committee is to manage the finances of
+
+the Gymkhana. It controls and supervises the expenditures incurred by the
+
+executive wings of the Gymkhana, viz. The Cultural Council, The Science & Technology 
+
+Council, The Games & Sports Council and The Film & Media Council.
+
+<h4>Emergency Committee</h4>
+
+Emergency Committee represents the Parliament in case of any emergency
+
+since the Parliament meeting cannot be called up in short time.
+
+<h4>Festivals Committee</h4>
+
+Festivals Committee is responsible for all matters except financial matters related to 
+
+festivals organized by Gymkhana. All preliminary reports related to festivals are 
+
+submitted to this committee.
+
+<h4>Nominations Committee</h4>
+
+The Nominations Committee is responsible for nominating students for various institute 
+
+bodies.
+
+<h4>Hostel Affairs & General Welfare Committee</h4>
+
+The Hostel Affairs & General Welfare Committee looks after the matters related to 
+
+hostels and general welfare of students.
+
+<h4>Security Committee</h4>
+
+The Security Committee serves as a student representative body to the Institute Proctorial 
+
+Board. It oversees security arrangements in campus at all times and address
+
+students’ concerns and issues regarding crime
+
+<h4>Students’ Placement Committee</h4>
+
+The primary aim of the Students’ Placement Committee is to express
+
+students’ opinion regarding placements and it acts as the students’ central
+
+advisory body for the Training and Placement Cell.
+
+<h4>Web Management Committee</h4>
+
+The Web Management Committee manages websites of Gymkhana, festivals and student 
+
+related activities. It is also  responsible for managing all content of website.
+
+Undergraduate Students’ Academic Affairs Committee
+
+It advices and assists the Parliament on matters related to the undergraduate
+
+academic program.
+
+Postgraduate Students’ Academic Affairs Committee
+
+It advices and assists the Parliament on matters related to postgraduate
+
+academic program.
+
+<h4>Alumni Relations Committee</h4>
+
+Alumni Relations Committee assists the Dean of Resources and Alumni Affairs in
+
+improving student-alumni interaction. It collects information regarding
+
+alumni and serves as a critical link between the Institute and alumni.
+
+<h4>Grievance Redressal and Enquiries Committee</h4>
+
+Grievance Redressal and Enquiries Committee addresses grievances of General Body and 
+
+acts as a source for providing information to the general body.
+
+</div>
+
+                </div><!--END Left hand side area-->
+
+
+
+            </div>
+        </div>
+
+
+        <?php include_once("footer.php"); ?>
+
+
+        <!-- JQuery JavaScript -->
+        <script src="assets/js/jquery.min.js"></script>
+        <!-- Bootstrap JavaScript -->
+        <script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
+        <!-- Custom JavaScript -->
+        <script src="assets/js/custom.js" type="text/javascript"></script>
+
+        <script src="assets/js/jquery.newsTicker.min.js" type="text/javascript"></script>
+        <script>
+            var nt_example1 = $('#parliament-ticker').newsTicker({
+                row_height: 70,
+                max_rows: 4,
+                duration: 3000000,
+                prevButton: $('#news-ticker-prev'),
+                nextButton: $('#news-ticker-next'),
+            });
+        </script>
+
+
+    </body>
+
+</html>
